a22cb77916f615e97ba6ee959ef72252fb30935b
nak-244 <t09024656860@gmail.com>
2023-06-12 10:14:03 +0900

    up

--- 01_latest_record/latest_record.js
+++ 01_latest_record/latest_record.js
@@ -1,27 +1,30 @@
 (function() {
   'use strict';
 
-  kintone.events.on('app.record.create.submit', function(event) {
-    var childRecord = event.record;
-    var parentAppId = 482; // 親アプリのApp IDを指定
+  function onAfterInsert(event) {
+    var record = event.record;
+    var cId = record.c_id.value;
 
-    var parentRecord = {
-      app: parentAppId,
-      record: {
-        // 親アプリに追加するレコードのフィールド値を子アプリからコピー
-        c_id: { value: childRecord.c_id.value },
-        c_name: { value: childRecord.c_name.value },
-        // 他のフィールドも同様にコピーする
+    var params = { app: event.appId, query: 'c_id = "' + cId + '"' };
+
+    kintone.api(kintone.api.url('/k/v1/records', true), 'GET', params, function(response) {
+      var records = response.records;
+      var deleteIds = [];
+
+      for (var i = 0; i < records.length; i++) {
+        if (records[i].$id.value !== event.recordId) {
+          deleteIds.push(records[i].$id.value);
+        }
       }
-    };
 
-    kintone.api(kintone.api.url('/k/v1/record', true), 'POST', parentRecord, function(resp) {
-      // 親アプリへのレコード追加が成功した場合の処理
-      console.log('Parent record added successfully:', resp);
-    }, function(error) {
-      // 親アプリへのレコード追加が失敗した場合の処理
-      console.error('Failed to add parent record:', error);
+      if (deleteIds.length > 0) {
+        var deleteParams = { app: event.appId, ids: deleteIds };
+        kintone.api(kintone.api.url('/k/v1/records', true), 'DELETE', deleteParams, function() {
+          console.log('古いレコードを削除しました。');
+        });
+      }
     });
-  });
+  }
 
+  kintone.events.on('app.record.create.submit.success', onAfterInsert);
 })();
